7bfc10f833416f6a36092e0940fc60eb404558e1
niccithenomad <nicci.abueg@gmail.com>
2016-12-15 11:56:02 -0800

    time display complete solution

--- RubyAssignments/Rails/timedisplay/app/controllers/application_controller.rb
+++ RubyAssignments/Rails/timedisplay/app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_time
+
+def set_time
+  @time = Time.now
+end
 end
